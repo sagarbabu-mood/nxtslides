9b12470a1e129fb77428d4b6b9eecea20a39ee19
sagarbabu <sagarbabusv180.com>
2024-02-20 08:58:17 +0000

    initial commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,14 @@
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {IoMoonOutline} from 'react-icons/io5'
-import {MdOutlineGTranslate} from 'react-icons/md'
-import {FaRegBell} from 'react-icons/fa'
 import './index.css'
 
-const Header = () => {
-  return (
-    <nav className="navbar">
-      <div className="my-dashboard">
-        <GiHamburgerMenu />
-        <p className="dashboard-txt">Dashboard</p>
-      </div>
-      <div className="nav-btn-container">
-        <button className="nav-btn">
-          <IoMoonOutline color="black" size={26} />
-        </button>
-
-        <button className="nav-btn">
-          <FaRegBell size={26} />
-        </button>
-        <button className="nav-btn">
-          <MdOutlineGTranslate size={26} />
-        </button>
-        <img
-          src="https://res.cloudinary.com/dzt6qmhmq/image/upload/v1708353635/man_f0gx4u.png"
-          alt=""
-          height={40}
-          className="avatar"
-        />
-      </div>
-    </nav>
-  )
-}
+const Header = () => (
+  <div className="header-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+      alt="nxt slides logo"
+      className="logo"
+    />
+    <h1 className="title">Nxt Slides</h1>
+  </div>
+)
 
 export default Header
